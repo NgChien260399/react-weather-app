aac5bd19308f9a695dca911b3981004b37cb7d5d
NgChien2603 <abulick113@gmail.com>
2023-10-04 08:34:57 +0700

    city search

---
+++ src/api.js
@@ -0,0 +1,8 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "17154a983cmsh1713bfdd386b384p14b9e2jsn3fcf682d437d",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
